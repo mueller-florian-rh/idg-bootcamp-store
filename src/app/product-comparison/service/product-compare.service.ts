222c7ad582ec4590ed1b92c429ff7626129329d5
Florian Mueller <florian.mueller07@sap.com>
2020-07-06 23:20:28 +0200

    Step 4: add service logic & persitence

--- src/app/product-comparison/service/product-compare.service.ts
+++ src/app/product-comparison/service/product-compare.service.ts
@@ -1,9 +1,62 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductCompareService {
 
-  constructor() { }
+  private maxNumberOfProducts = 3;
+
+  private productSet: Set<string> = new Set<string>();
+
+  private sessionKey = "product-set";
+
+  public addProduct(productCode) {
+    if (this.productSet.size <= this.maxNumberOfProducts && !this.productSet.has(productCode)) {
+      this.productSet.add(productCode);
+      this.saveToSession()
+    }
+  };
+
+  public removeProduct(productCode) {
+    this.productSet.delete(productCode);
+    this.saveToSession()
+  }
+
+  public clearComparison() {
+    this.productSet.clear();
+    this.saveToSession()
+  }
+
+  public canAddProduct() {
+    return this.productSet.size <= this.maxNumberOfProducts;
+  }
+
+  getNumberOfProduct(): number {
+    return this.productSet.size;
+  }
+
+  isProductInComparison(productCode: string): boolean {
+    return this.productSet.has(productCode);
+  }
+
+  canShowCompareView(): boolean {
+    return this.productSet.size > 1;
+  }
+
+  getProductCodes(): Set<string> {
+    return this.productSet;
+  }
+
+  constructor() {
+    if (localStorage.getItem(this.sessionKey) != null) {
+      this.productSet = new Set<string>(JSON.parse(localStorage.getItem(this.sessionKey)));
+    } else {
+      this.productSet = new Set<string>();
+    }
+  }
+
+  saveToSession() {
+    localStorage.setItem(this.sessionKey, JSON.stringify(Array.from(this.productSet)));
+  }
 }
