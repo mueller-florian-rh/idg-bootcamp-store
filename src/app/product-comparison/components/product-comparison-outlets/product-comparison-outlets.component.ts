1aeaa584ad6adb338dc7a4a8fc805414ec0e19d8
Florian Mueller <florian.mueller07@sap.com>
2020-07-07 20:43:30 +0200

    Step 6: implement outlets

--- src/app/product-comparison/components/product-comparison-outlets/product-comparison-outlets.component.ts
+++ src/app/product-comparison/components/product-comparison-outlets/product-comparison-outlets.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductCompareService} from "../../service/product-compare.service";
+import {ProductDetailOutlets} from "@spartacus/storefront";
 
 @Component({
   selector: 'app-product-comparison-outlets',
@@ -7,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComparisonOutletsComponent implements OnInit {
 
-  constructor() { }
+  constructor(protected productComparisonService: ProductCompareService) { }
+
+  pdpoutlets = ProductDetailOutlets;
+
+  addProduct(productCode: string)
+  {
+    this.productComparisonService.addProduct(productCode);
+  }
+
+  removeProduct(productCode: string)
+  {
+    this.productComparisonService.removeProduct(productCode);
+  }
+
+  canAddProduct(productCode: string)
+  {
+    if(this.productComparisonService.canAddProduct() && !this.productComparisonService.isProductInComparison(productCode))
+    {
+      return true;
+    }
+
+    return false;
+  }
+
+  isProductInComparison(productCode: string)
+  {
+    return this.productComparisonService.isProductInComparison(productCode);
+  }
+
+  getNumberOfProducts() : number
+  {
+    return this.productComparisonService.getNumberOfProduct();
+  }
+
+  canShowCompareView() : boolean
+  {
+    return this.productComparisonService.canShowCompareView();
+  }
 
   ngOnInit(): void {
   }
 
+
+  clearComparison() {
+    this.productComparisonService.clearComparison();
+  }
 }
--- src/app/product-comparison/product-comparison.module.ts
+++ src/app/product-comparison/product-comparison.module.ts
@@ -3,6 +3,8 @@ import {CommonModule} from '@angular/common';
 import {ProductComparisonOutletsComponent} from './components/product-comparison-outlets/product-comparison-outlets.component';
 import {OutletRefModule} from "@spartacus/storefront";
 import {ProductComparisonTableComponent} from './components/product-comparison-table/product-comparison-table.component';
+import {UrlModule} from "@spartacus/core";
+import {RouterModule} from "@angular/router";
 
 
 @NgModule({
@@ -11,7 +13,9 @@ import {ProductComparisonTableComponent} from './components/product-comparison-t
   bootstrap: [ProductComparisonOutletsComponent, ProductComparisonTableComponent],
   imports: [
     CommonModule,
-    OutletRefModule
+    OutletRefModule,
+    UrlModule,
+    RouterModule
   ]
 })
 export class ProductComparisonModule {
